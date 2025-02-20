48d321a5bbeccbfaef6d0740cd1b2e3c788a630f
Vaidas Simkus <vaidas@simkus.co.uk>
2025-02-20 22:28:14 +0000

    initial commit

---
+++ server/server.js
@@ -0,0 +1,18 @@
+// server.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('CV Builder API is running...');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
